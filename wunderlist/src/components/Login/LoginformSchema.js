047272a676348b297e36bb7a7d0a9095c25482c5
unknown <aelise17264@gmail.com>
2020-08-25 10:54:48 -0600

    up to date w/ master

--- wunderlist/src/components/Login/LoginformSchema.js
+++ wunderlist/src/components/Login/LoginformSchema.js
@@ -3,6 +3,8 @@ import * as yup from 'yup'
 const formSchema = yup.object().shape({
     username: yup
       .string()
+      .min(6, "Username must be at least 6 characters long.")
+
       .required("Must include username."),
     
     password: yup
