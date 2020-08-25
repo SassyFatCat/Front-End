d5b7851a1cbdb851287053b5be93c0db140150aa
Ryan Lee <therealsassyfatcat@outlook.com>
2020-08-24 19:10:29 -0500

    array to checkbox conversion

--- wunderlist/src/components/AddEditForm/AddEditForm.js
+++ wunderlist/src/components/AddEditForm/AddEditForm.js
@@ -31,18 +31,19 @@ useEffect(() => {
 if (todosContext.addEdit.is === 'edit') {
     // make a axiosWithAuth().get, getting the todo task with the id in todosContext.addEdit.id
     // Populate the form with the success data
-    const todoItem = dummyData.find(item => item.id === todosContext.addEdit.id);
-    const checkboxTags = {
+    const todoItem = {...dummyData.find(item => item.id === todosContext.addEdit.id)};
+    let checkboxTags = {
         school: false,
         exercise: false,
         work: false
     };
     todoItem.tags.forEach(tag => checkboxTags[tag] = true);
-    // todoItem.tags = checkboxTags;
+    todoItem.tags = checkboxTags;
     setFormData(todoItem);
+    console.log(todosContext.todos)
 }
 
-}, )
+}, [todosContext.addEdit.id])
     return (
         <div>
             <h1>AddEditForm</h1>
