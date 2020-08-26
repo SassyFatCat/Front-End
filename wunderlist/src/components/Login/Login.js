a939b09aff77d80a39c4a8d43d1931fd7afe3be5
unknown <aelise17264@gmail.com>
2020-08-26 11:29:30 -0600

    updated styles

--- wunderlist/src/components/Login/Login.js
+++ wunderlist/src/components/Login/Login.js
@@ -1,5 +1,5 @@
 
-import React, {useState, useEffect, useMeasure} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Link } from 'react-router-dom';
 import axios from 'axios';
 import * as yup from 'yup';
